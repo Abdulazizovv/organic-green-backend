8e777176246a09176bcf33601e051abe6080965e
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2025-09-05 19:39:47 +0500

    added cors

--- core/settings.py
+++ core/settings.py
@@ -66,6 +66,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://organicgreen.uz/",
+    "https://organicgreen.uz/"
 ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
